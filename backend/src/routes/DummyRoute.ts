227fd05e724ee543ced5649db9b2c1a00979c651
Vince Kevin Añana <152976699+mantequilla45@users.noreply.github.com>
2025-10-11 13:06:38 +0800

    update

--- backend/src/routes/DummyRoute.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getDummyData } from "../controllers/DummyController";
-
-const router = Router();
-
-router.get('/', getDummyData);
-
-export default router;
--- mobile/src/navigation/types.ts
+++
@@ -1,9 +0,0 @@
-export type AuthStackParamList = {
-    Login: undefined;
-    Register: undefined;
-};
-
-export type AppStackParamList = {
-    Dashboard: undefined;
-    Screen1: undefined;
-};
