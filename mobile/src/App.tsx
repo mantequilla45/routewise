46baddab69a2338b0c22e1c22e44c10879f02e94
Vince Kevin Añana <152976699+mantequilla45@users.noreply.github.com>
2025-10-13 01:10:00 -0400

    login

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import LoginScreen from './screens/LoginScreen';
+import OnboardingScreen from './screens/OnboardingScreen';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -9,7 +9,7 @@ function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <LoginScreen />
+      <OnboardingScreen />
     </SafeAreaProvider>
   );
 }
