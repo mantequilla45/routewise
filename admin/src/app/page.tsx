592d5ac79e4bd490fb57a7fa02ebd899d43f10fc
MarkToroy <markjohntoroy2016@gmail.com>
2025-10-26 00:29:40 +0800

    admin login page ui/ux

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -2,101 +2,63 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-12 gap-8">
+      <main className="border-2 p-5 rounded-xl -20 flex flex-col gap-[15px] row-start-2 items-center sm:items-center">
+        <div className="flex flex-col items-center sm:items-center">
+          <p className="p-2">
+            Welcome to
+          </p>
+          <Image
+            src="/routewise.svg"
+            alt="Routewise logo"
+            width={180}
+            height={38}
+            priority
+          />
+          <p className="pt-5">
+            Log in using an admin account
+          </p>
         </div>
+        
+        <form className="flex flex-col w-90 mb-5">
+          <p>
+            Email:
+          </p>
+          <input 
+            type="email"
+            required
+            // placeholder="email"
+            className="mt-1 p-2 border rounded-md border-gray-400 w-full max-w-sm">
+          </input>
+
+          <p className="mt-4">
+            Password:
+          </p>
+          <input 
+            type="password"
+            required
+            // placeholder="password"
+            className="mt-1 p-2 border rounded-md border-gray-400 w-full max-w-sm">
+          </input>
+
+          <button 
+            type="submit"
+            className="flex flex-col mt-6 p-3 border rounded-md border-gray-700 w-full max-w-sm font-bold hover:bg-gray-700 cursor-pointer"
+            >
+              Log In
+          </button>
+        </form>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+      <footer className="row-start-3 flex gap-[24px] flex-col items-center justify-center">
+        <div
+          className="gap-2 w-95 sm:text-center"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          By signing up, you agree to the
+          <span className="font-bold"> Terms of Service </span>
+          and
+          <span className="font-bold"> Data Processing Agreement</span>
+        </div>
       </footer>
     </div>
   );
