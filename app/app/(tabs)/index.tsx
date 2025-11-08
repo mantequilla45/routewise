8e00a8cf7e36a3493b12df1f35e3cfd65e0745b2
Vince Kevin Añana <152976699+mantequilla45@users.noreply.github.com>
2025-11-08 16:39:26 -0500

    update

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,11 +1,18 @@
 import { AppleMaps, GoogleMaps } from 'expo-maps';
 import { Platform, Text } from 'react-native';
 
-export default function App() {
+export default function Index() {
+  const initialRegion = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
   if (Platform.OS === 'ios') {
-    return <AppleMaps.View style={{ flex: 1 }} />;
+    return <AppleMaps.View style={{ flex: 1 }} initialRegion={initialRegion} />;
   } else if (Platform.OS === 'android') {
-    return <GoogleMaps.View style={{ flex: 1 }} />;
+    return <GoogleMaps.View style={{ flex: 1 }} initialRegion={initialRegion} />;
   } else {
     return <Text>Maps are only available on Android and iOS</Text>;
   }
