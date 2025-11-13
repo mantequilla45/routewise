1a396406f0a7b8eeba9127fece57cacd5eb5a613
LAPTOP-9IAHD3O6\rhongo pc <epicjeff03@gmail.com>
2025-11-14 01:08:52 +0800

    feature: map on index screen

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,34 +1,32 @@
-import LoginForm from '@/components/LoginForm';
-import { useAuth } from '@/context/auth';
 import { AppleMaps, GoogleMaps } from 'expo-maps';
-import { ActivityIndicator, Button, Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
 
-  return (
-
-    <View>
-      <Text>
-
-        Home Screen
-      </Text>
-    </View>
-  );
-
-  {/*  if (Platform.OS === 'ios') {
+  if (Platform.OS === 'android') {
+    return (
+      <View style={styles.container}>
+        <GoogleMaps.View
+          style={styles.map}
+          cameraPosition={{
+            coordinates: { latitude: 10.3157, longitude: 123.8854 },
+            zoom: 12,
+          }}
+        />
+      </View>
+    );
+  } else if (Platform.OS === 'ios') {
     return <AppleMaps.View style={{ flex: 1 }} />;
-  } else if (Platform.OS === 'android') {
-    return <GoogleMaps.View style={{ flex: 1 }} />;
   } else {
     return <Text>Maps are only available on Android and iOS</Text>;
   }
-} */}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
+  },
+  map: {
+    flex: 1,
+  },
 });
