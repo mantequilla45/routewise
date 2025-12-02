7752b05bb0cd16ad9ea27898c7afddff0fd97b47
MarkToroy <markjohntoroy2016@gmail.com>
2025-12-02 21:59:18 +0800

    real auth

--- admin/src/lib/supabase/middleware.ts
+++ admin/src/lib/supabase/middleware.ts
@@ -40,6 +40,7 @@ export async function updateSession(request: NextRequest) {
   // with the Supabase client, your users may be randomly logged out.
   const { data } = await supabase.auth.getClaims();
   const user = data?.claims;
+
   const pathname = request.nextUrl.pathname;
 
   if (pathname !== "/" && !user && !pathname.startsWith("/auth")) {
